82ed1f4de12872ad10e1de2d264b50f5eba5587e
Eyeaadil <madil9227583@gmail.com>
2024-05-17 21:20:18 +0530

    Creating a store

--- src/component/Browse.jsx
+++ src/component/Browse.jsx
@@ -1,12 +1,15 @@
 import React, { useEffect } from 'react'
 import Header from './Header'
 import { API_OPTIONS } from '../utils/constant'
+import { useDispatch } from 'react-redux'
+import { addNowPlayingMovies } from '../utils/moviesSlice'
 
 const Browse = () => {
+  const dispatch = useDispatch();
   const getNowPlayingMovies = async()=>{
      const data = await fetch('https://api.themoviedb.org/3/movie/now_playing?page=1',API_OPTIONS)
      const JsonData = await data.json();
-     console.log(JsonData)
+     dispatch(addNowPlayingMovies(JsonData.results));
   }
 
   useEffect(()=>{
